763d733bf7725c8bd8b918e643eda8c1e803e589
Prof. Marcos Santos <marcos.santos@atitus.edu.br>
2024-10-29 21:01:44 -0300

    versão base do codigo

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-print("Hello World!")
+import pygame
+pygame.init()
+tamanho = (1000,592)
+clock = pygame.time.Clock()
+tela = pygame.display.set_mode( tamanho )
+pygame.display.set_caption("Corrida Maluca")
+branco = (255,255,255)
+preta = (0,0,0)
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            quit()
+   
+    tela.fill( branco )
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
+    
 
-print(5 + 10)
