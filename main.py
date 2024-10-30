4362168f25cb3f918d5ce8e33d187697b30a1c12
Prof. Marcos Santos <marcos.santos@atitus.edu.br>
2024-10-29 21:10:30 -0300

    carrinhos adicionados

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ tela = pygame.display.set_mode( tamanho )
 pygame.display.set_caption("Corrida Maluca")
 branco = (255,255,255)
 preta = (0,0,0)
+fundo = pygame.image.load("assets/fundo.png")
+carro1 = pygame.image.load("assets/carro1.png")
+carro2 = pygame.image.load("assets/carro2.png")
+
+
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             quit()
    
     tela.fill( branco )
+    tela.blit(fundo, (0,0))
+    tela.blit(carro1, (0,50))
+    tela.blit(carro2, (0,180))
+    
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
