a9c75eda337111609f71afd7452ce1180c689f69
Prof. Marcos Santos <marcos.santos@atitus.edu.br>
2024-10-29 21:39:24 -0300

    Base da corrida pronta

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 tamanho = (1000,592)
 clock = pygame.time.Clock()
@@ -10,7 +11,15 @@ fundo = pygame.image.load("assets/fundo.png")
 carro1 = pygame.image.load("assets/carro1.png")
 carro2 = pygame.image.load("assets/carro2.png")
 
-
+movXCar1 = 0
+movXCar2 = 0
+posYCar1 = 50
+posYCar2 = 180
+vitoria = pygame.mixer.Sound("assets/vitoria.mp3")
+vitoria.set_volume(0.5)
+pygame.mixer.music.load("assets/trilha.mp3")
+pygame.mixer.music.play(-1) #-1 looping, 1,2 3 vezes
+acabou = False
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -18,8 +27,38 @@ while True:
    
     tela.fill( branco )
     tela.blit(fundo, (0,0))
-    tela.blit(carro1, (0,50))
-    tela.blit(carro2, (0,180))
+    tela.blit(carro1, (movXCar1,posYCar1))
+    tela.blit(carro2, (movXCar2,posYCar2))
+    
+    if not acabou :
+        movXCar1 = movXCar1 + random.randint(0,10)
+        movXCar2 = movXCar2 + random.randint(0,10)
+    
+    if movXCar1 > 1000:
+        movXCar1 = 0
+        posYCar1 = 350
+        
+    if movXCar2 > 1000:
+        movXCar2 = 0
+        posYCar2 = 480
+    
+    fonte = pygame.font.Font("freesansbold.ttf",60)
+    textoVermelho = fonte.render("Vermelho Ganhou!", True, branco)
+    textoAmarelo = fonte.render("Amarelo Ganhou!", True, branco)
+    
+    if posYCar1 == 350 and movXCar1 >= 900 and movXCar1 > movXCar2:
+        tela.blit(textoVermelho, (270,70))
+        acabou = True
+        pygame.mixer.music.stop()
+        pygame.mixer.Sound.play(vitoria)
+        
+    elif posYCar2 == 480 and movXCar2 >= 900 and movXCar2 > movXCar1:
+        tela.blit(textoAmarelo, (270,180))
+        acabou = True
+        pygame.mixer.music.stop()
+        pygame.mixer.Sound.play(vitoria)
+    
+    
     
     pygame.display.update()
     clock.tick(60)
