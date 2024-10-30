111976fceb1bbd2ae0babef1c59665cea6273ebb
Prof. Marcos Santos <marcos.santos@atitus.edu.br>
2024-10-30 19:38:22 -0300

    melhoras no som e icone da aplicação

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ pygame.init()
 tamanho = (1000,592)
 clock = pygame.time.Clock()
 tela = pygame.display.set_mode( tamanho )
+icone = pygame.image.load("assets/icone.ico")
+pygame.display.set_icon(icone)
 pygame.display.set_caption("Corrida Maluca")
 branco = (255,255,255)
 preta = (0,0,0)
@@ -20,8 +22,9 @@ vitoria.set_volume(0.5)
 pygame.mixer.music.load("assets/trilha.mp3")
 pygame.mixer.music.play(-1) #-1 looping, 1,2 3 vezes
 acabou = False
+somDaVitoria = False
 while True:
-    for evento in pygame.event.get():
+    for evento in pygame.event.get(): 
         if evento.type == pygame.QUIT:
             quit()
    
@@ -33,6 +36,12 @@ while True:
     if not acabou :
         movXCar1 = movXCar1 + random.randint(0,10)
         movXCar2 = movXCar2 + random.randint(0,10)
+    else:
+        pygame.mixer.music.stop()
+        if somDaVitoria == False:
+            pygame.mixer.Sound.play(vitoria)
+            somDaVitoria = True
+        
     
     if movXCar1 > 1000:
         movXCar1 = 0
@@ -49,15 +58,10 @@ while True:
     if posYCar1 == 350 and movXCar1 >= 900 and movXCar1 > movXCar2:
         tela.blit(textoVermelho, (270,70))
         acabou = True
-        pygame.mixer.music.stop()
-        pygame.mixer.Sound.play(vitoria)
         
     elif posYCar2 == 480 and movXCar2 >= 900 and movXCar2 > movXCar1:
         tela.blit(textoAmarelo, (270,180))
         acabou = True
-        pygame.mixer.music.stop()
-        pygame.mixer.Sound.play(vitoria)
-    
     
     
     pygame.display.update()
